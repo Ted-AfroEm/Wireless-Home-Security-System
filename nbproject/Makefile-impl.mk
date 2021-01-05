58efb57892045f45e8eb8bd93bf00e70855424da
Ted-AfroEm <wedikahsu122@gmail.com>
2021-01-05 23:38:57 +0300

    Modify file name from 001UART to Wireless-Home-Security-System

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=001UART_Blink.X
+PROJECTNAME=Wireless-Home-Security-System
 
 # Active Configuration
 DEFAULTCONF=default
