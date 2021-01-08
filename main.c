882341c2075433ce15b55cb7a37d0dfbd7fbcadd
Ted-AfroEm <wedikahsu122@gmail.com>
2021-01-09 02:11:13 +0300

    Modified so it can make a call using GSM sim800l module

--- main.c
+++ main.c
@@ -16,15 +16,16 @@
 //#include <stdio.h>
 #include <stdint.h>
 //#include <stdbool.h>
-//#include "string.h"
+#include "string.h"
 
 #include "device_Config.h"
 #include "IO.h"
 #include "Initialize.h"
 #include "UART.h"
+#include "gsmSim800l.h"
 #include "Interrupt.h"
 
-#define _XTAL_FREQ 4000000
+
 
 void main(void) {
     
@@ -39,18 +40,73 @@ void main(void) {
     UART1_Send_Greeting("What's UP");
     UART1_Send_Greeting("Congrats....UART have successfully send string");
     
-    LATB = 0x00;
-     while(1)
+    u8 i;
+    int ret;
+	
+        for(i = 0;i < STABLE_TIMES;i++)
+        {
+                __delay_ms(1000);
+        }
+    
+    while(1)
     {
-        UART1_Send_Greeting("LED ON");
-       LED_1_SetHigh();
-       LED_3_SetHigh();
-       __delay_ms(1000);  
-       UART1_Send_Greeting("LED OFF");
-       LED_1_SetLow() ;
-       LED_3_SetLow() ;
-       __delay_ms(1000); 
-      
+	__delay_ms(500);       
+        LED_1_Toggle();
+
+		if(KEY1()== 0)
+            
+			{		
+            __delay_ms(100); 
+			
+			if(KEY1()== 0)
+			{
+				while(!KEY1());
+				
+				ret = call_phone_num(phone_num);
+
+				if(ret == 1)    //1
+				{
+                    LATB = 0x1;
+                    UART1_Send_Greeting("SUCCESS");
+                   //success
+				}
+				else if(ret == -1) //2
+				{
+                    LATB = 0x2;
+                    UART1_Send_Greeting("COMMUNITE_ERROR");
+                    //COMMUNITE_ERROR 
+				}
+				else if(ret == -2) //3
+				{
+                    LATB = 0x3;
+                    UART1_Send_Greeting("NO_SIM_CARD_ERROR");
+                    //NO_SIM_CARD_ERROR
+                    
+				}
+				else if(ret == -3) //4
+				{
+                    LATB = 0x4;
+                    UART1_Send_Greeting("SIM_CARD_NO_REG_ERROR");
+                    //SIM_CARD_NO_REG_ERROR 
+                }
+				else if(ret == -4)  //5
+				{
+                    LATB = 0x5;
+				}
+				else    //6
+				{
+                    LATB = 0x6;
+				}
+				
+                __delay_ms(3000);
+                LATB = 0x00;
+                
+				
+            }
+        }
+	
+	
+        
     }
   
     //return;
