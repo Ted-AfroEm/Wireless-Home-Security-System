f921f429baa0136a73940b1c048622dd1388991f
Ted-AfroEm <wedikahsu122@gmail.com>
2021-01-12 20:58:17 +0300

    Modifed get_data_from_server to set up HTTP get request

--- HTTP_Service.h
+++ HTTP_Service.h
@@ -96,10 +96,35 @@ int get_data_from_server(char *URL)
 {
     int ret;
 
-    ret = activate_bearer();
+   ret = activate_bearer();
     if(ret==0){
         return AT_SAPBR_ERROR;
     }
+    //Initialize HTTP Service 
+    ret = UART1_Send_AT_Command("AT+HTTPINIT","OK",2,50);
+    if(ret==0){
+        return AT_HTTPINTI_ERROR;
+    }
+    //Set HTTP to Use SSL(secure sockets layer) Function
+    ret = UART1_Send_AT_Command("AT+HTTPSSL=1","OK",2,50);
+    if(ret==0){
+        return AT_HTTPSSL_ERROR;
+    }
+    //Set HTTP Bearer profile identifier
+    ret = UART1_Send_AT_Command("AT+HTTPPARA=\"CID\",1","OK",2,50);
+    if(ret==0){
+        return AT_HTTPPARA_ERROR;
+    }
+    
+    memset(Server_URL,'\0',90);
+    strcpy(Server_URL,"AT+HTTPPARA=\"URL\",");
+    strcat(Server_URL, URL);
+    //Set HTTP URL
+    ret = UART1_Send_AT_Command(Server_URL,"OK",3,50*2);
+    if(ret==0){
+        return AT_HTTPARA_URL_ERROR;
+    }
+    
     return ret;
      
 }
