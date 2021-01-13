e1abd919c6072e3307a187a0f2056b14e96d1f1c
Ted-AfroEm <wedikahsu122@gmail.com>
2021-01-14 01:43:58 +0300

    made RB3 input pin for RF_Receiver

--- Initialize.h
+++ Initialize.h
@@ -36,7 +36,8 @@ void OSCILLATOR_Initialize(void)
 
 void PIN_Initialize(void)
 {
-    TRISB = 0xF0;       //RB3,RB2,RB1,RB0 - are LED outputs
+    TRISB = 0xF8;       //RB2,RB1,RB0 - are LED outputs
+                        //RB3 - input for RF_Receiver
     TRISC = 0xBF;       //RC7 - input(RX) : RC6 - output(TX) //set RC5 as input
                         //RC0 - input for IR_Pin
       
