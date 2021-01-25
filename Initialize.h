9687ad65e2a4d3ab78903a9a2567e6a0ab2875b7
Ted-AfroEm <wedikahsu122@gmail.com>
2021-01-25 05:34:54 +0300

    RB2 - input for magnetic contact sensor

--- Initialize.h
+++ Initialize.h
@@ -3,7 +3,7 @@ typedef 	char	u8;
 typedef 	int	u16;
 typedef 	long	u32;
 
-#define Buf_Max 		90
+#define Buf_Max 		200
 #define STABLE_TIMES	  	10
 
 char Uart1_Buf[Buf_Max];
@@ -12,6 +12,9 @@ uint8_t First_Int = 0;
 char Server_URL[90];
 char getHTTPBuffer[Buf_Max];    //Store data from Server
 char getDataSize[3];            //Store getHTTP data size
+
+char getToken[70];
+char token[] = "aastumnbvcxzlkj";
 
 struct
 {
@@ -36,13 +39,13 @@ void OSCILLATOR_Initialize(void)
 
 void PIN_Initialize(void)
 {
-    TRISB = 0xD9;       //RB2,RB1 - are LED outputs
+    TRISB = 0xDD;       //RB2 - input for magnetic contact sensor
                         //RB3 - input for RF_Receiver
                         //RB0 - input for PIR Pin
                         //RB5 - output for Buzzer
     TRISC = 0xBF;       //RC7 - input(RX) : RC6 - output(TX) //set RC5 as input
                         //RC0 - input for IR_Pin
-    //TRISDbits.TRISD7 = 0x0; //GSM Reset pin
+    //TRISDbits.TRISD7 = x0; //GSM Reset pin
     TRISD = 0xFC;       //RD0,RD1 - are LED outputs
     
     ANSELC = 0x1F;      //RC7(TX),RC6(RX) - are Digital 00111111 //set RC5 Digital
